019983bc8b231e5a67a549bb9672041beb7c6712
Eln Far <farzaliyevelnur00@gmail.com>
2023-08-31 20:17:13 +0400

    small big in not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className=' h-screen w-full text-center flex flex-col items-center justify-center bg-red-50'>
-      <h1 className='md:text-[3rem]'>Either this page is under construction <br /> or you don't have permission to see it</h1>
-      <Link href="/" className='text-blue-400'>Return Home</Link>
+      <h1 className='md:text-[3rem]'>Either this page is under construction <br /> or you do not have permission to see it</h1>
+      <Link href="/" className='text-blue-400'>Home</Link>
     </div>
   )
 }
